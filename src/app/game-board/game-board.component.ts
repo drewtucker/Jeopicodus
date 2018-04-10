3d314085f50ed8876e5a8e43bc80427ddd9b632f
Guest User <Guest@epicodus-3.local>
2018-04-10 08:53:05 -0700

    Merge with newly-created game board.

--- src/app/game-board/game-board.component.ts
+++ src/app/game-board/game-board.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, Output, OnInit } from '@angular/core';
 import { Router, Routes, RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-game-board',
@@ -8,9 +10,27 @@ import { Router, Routes, RouterModule } from '@angular/router';
 })
 export class GameBoardComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  // CATEGORY API CALLS
+  // https://opentdb.com/api.php?amount=5&category={{randomCategory}}&difficulty=easy&type=multiple
+
+
 }
+
+$(document).ready(function() {
+
+  let randomCategory = Math.floor(Math.random() * 24) + 9;
+
+  $.get(`https://opentdb.com/api.php?amount=5&category=${randomCategory}&difficulty=easy&type=multiple`).then(function(response) {
+    console.log(response.results[0]);
+    $("#results").text($("<div/>").html(response.results[0].question).text());
+  }).fail(function(error) {
+  console.log(error);
+});
+
+});
